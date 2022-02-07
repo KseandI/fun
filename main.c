fa522c93a63465a48db0bf258c7d74f73b2044de
c4llv07e <kseandi@gmail.com>
2022-02-07 21:18:48 +0300

    some cool functions, optimization and so on
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ typedef struct GameSystem
 
 typedef struct GameOptions
 {
+  Bool is_game_running;
   Bool dev_mode;
   Bool wireframe_mode;
 } GameOptions;
@@ -40,6 +41,42 @@ debug_startup_info(None)
             GLFW_VERSION_MINOR,
             GLFW_VERSION_REVISION);
   return;
+}
+
+None
+game_reload_wireframe_mode(None)
+{
+  if (game_options == null || game_system == null) /* if not inited game */
+    return ;
+    
+  if (game_options->wireframe_mode != 0x0)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); /* don't fill polygons */
+  else
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); /* fill polygons */
+
+  return ;
+}
+
+None
+game_reload_all(None)
+{
+  game_reload_wireframe_mode();
+  return ;
+}
+
+None
+game_toggle_wireframe_mode(None)
+{
+  if (game_options == null)     /* if options not inited */
+    return ;
+  if (game_system == null)      /* if glew not inited */
+    return ;
+  
+  game_options->wireframe_mode = !(game_options->wireframe_mode); /* toggle options */
+
+  game_reload_wireframe_mode();
+  
+  return ;
 }
 
 void
@@ -60,10 +97,20 @@ glfw_key_handle(GLFWwindow* win, Int key, Int scancode, Int action, Int mods)
   if (win != game_system->window) /* if inputed on another window */
     return ;
 
-  if (key == GLFW_KEY_W && action == GLFW_PRESS)
+  if (action == GLFW_PRESS)
     {
-      game_options->wireframe_mode = !(game_options->wireframe_mode);
-      return ;
+      switch (key)
+        {
+        case GLFW_KEY_W:        /* toggle wireframe mode */
+          game_toggle_wireframe_mode();
+          break;
+        case GLFW_KEY_Q:        /* quit game */
+          game_options->is_game_running = false;
+          break;
+        default:
+          break;
+        }
+      return;
     }
   
   return ;
@@ -85,17 +132,17 @@ game_system_init(None)
       return error_out_of_mem;
     }
 
-  glfwSetErrorCallback(glfw_error_handle);
+  glfwSetErrorCallback(glfw_error_handle); /* set function/callback for glfw error handle */
   
-  if (glfwInit() == 0x0)
+  if (glfwInit() == 0x0)        /* init */
     {
       PRT_ERROR("error, can't init glfw\n");
       return error_lib;
     }
 
-  ON_DEBUG(glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE));
+  ON_DEBUG(glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE)); /* enable opengl debug context (IDK what it is) */
   
-  game_system->window = glfwCreateWindow(640, 480, "Fun", null, null);
+  game_system->window = glfwCreateWindow(640, 480, "Fun", null, null); /* create window */
   if (game_system->window == null)
     {
       PRT_ERROR("error, can't create window\n");
@@ -103,17 +150,17 @@ game_system_init(None)
       return error_system;
     }
 
-  glfwMakeContextCurrent(game_system->window);
-  glfwSetKeyCallback(game_system->window, glfw_key_handle);
+  glfwMakeContextCurrent(game_system->window); /* select context */
+  glfwSetKeyCallback(game_system->window, glfw_key_handle); /* set function for key input handle */
 
-  glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK)
+  glewExperimental = GL_TRUE;   /* enable experimental function */
+  if (glewInit() != GLEW_OK)    /* get new opengl functions */
     {
       PRT_ERROR("error, can't init glew\n");
       return error_lib;
     }
 
-  glViewport(0, 0, 640, 480);
+  glViewport(0, 0, 640, 480);   /* pass window size to opengl */
   
   return error_none;
 }
@@ -121,16 +168,16 @@ game_system_init(None)
 Int
 game_system_terminate(None)
 {
-  if (game_system == null)
+  if (game_system == null)      /* if system already terminated */
     {
       PRT_WARN("warning, tried to terminate uninitalized system\n");
       return error_already_done;
     }
   
-  glfwDestroyWindow(game_system->window);
-  glfwTerminate();
+  glfwDestroyWindow(game_system->window); /* terminate main window */
+  glfwTerminate();                        /* terminate glfw render */
 
-  free(game_system);
+  free(game_system);            /* de-allocate system struct */
   
   return error_none;
 }
@@ -138,13 +185,13 @@ game_system_terminate(None)
 Int
 game_options_init(None)
 {
-  game_options = new(GameOptions);
-  if (game_options == null)
+  game_options = new(GameOptions); /* allocate new options */
+  if (game_options == null)        /* if out of memory */
     {
       PRT_ERROR("error, can't allocate memory for options (rly?)\n");
       return error_out_of_mem;
     }
-  memset(game_options, 0x0, sizeof(GameOptions));
+  memset(game_options, 0x0, sizeof(GameOptions)); /* clear structure */
 
   return error_none;
 }
@@ -152,12 +199,12 @@ game_options_init(None)
 Int
 game_options_terminate(None)
 {
-  if (game_options == null)
+  if (game_options == null)     /* if already deallocated */
     {
       PRT_WARN("warning, tried to terminate uninitalized options\n");
       return error_already_done;
     }
-  free(game_options);
+  free(game_options);           /* deallocate options */
   return error_none;
 }
 
@@ -166,68 +213,69 @@ get_file_size(FILE* file)
 {
   uint32_t current_pos;
   uint32_t result;
-  if (file == null)
+  if (file == null)             /* if file doesn't exists */
     {
       PRT_DEBUG("tried to get size of null-file\n");
       return error_data;
     }
-  current_pos = ftell(file);
-  fseek(file, 0x0, SEEK_END);
-  result = ftell(file);
-  fseek(file, current_pos, SEEK_SET);
+  current_pos = ftell(file);    /* get current cursor position */
+  fseek(file, 0x0, SEEK_END);   /* go to the end */
+  result = ftell(file);         /* get cursor position */
+  fseek(file, current_pos, SEEK_SET); /* return to the original position */
   return result;
 }
 
 GLuint
 read_shader(GLenum type, const char* path)
 {
-  Int error_code;
-  Char error_message[0x200];
-  FILE* fd;
-  char* source;
-  uint32_t shader_size;
-  GLuint shader;
+  Int error_code;               /* compilation error code */
+  Char error_message[0x200];    /* compilation error message */
+  FILE* fd;                     /* file descriptor (system's file abstraction) */
+  char* source;                 /* shader's code */
+  uint32_t shader_size;         /* size of shader */
+  GLuint shader;                /* pointer to the shader */
 
-  shader = glCreateShader(type);
-  fd = fopen(path, "r");
+  shader = glCreateShader(type); /* create new shader */
+  fd = fopen(path, "r");         /* open source file */
   
-  if (fd == null)
+  if (fd == null)               /* if file doesn't exists */
     {
       PRT_WARN("warning, can't open vertex shader \"%s\"\n", path);
       
-    shader_fallback:
-      source = "";
+    shader_fallback:            /* use empty shader */
+      source = "#version 330 core\n";
       shader_size = 0x0;
     }
   else
     {
-      shader_size = get_file_size(fd);
+      shader_size = get_file_size(fd); /* get shader size */
       
-      source = (char*) malloc(sizeof(char)*shader_size);
-      memset(source, 0x0, shader_size);
+      source = (char*) malloc(sizeof(char)*shader_size); /* allocate memory for shader source */
       
-      if (source == null)
+      if (source == null)       /* if can't allocate memory */
         {
           PRT_ERROR("error, can't allocate memory for shader\n");
           goto shader_fallback;
         }
+      memset(source, 0x0, shader_size);                  /* clear this memory */
       
       /* It just works =| */
-      while (fgets(source+strlen(source), shader_size, fd) != 0x0) {}
+      while (fgets(source+strlen(source), shader_size, fd) != 0x0) {} /* read shader to the string */
       
-      fclose(fd);
+      fclose(fd);               /* close file */
     }
   
 #ifdef PRINT_SHADERS
-  PRT_DEBUG("Shader(%u): \"%s\"\n", shader_size, source);
+  PRT_DEBUG("Shader(%u): \"%s\"\n", shader_size, source); /* print shader source if required */
 #endif /* PRINT_SHADERS */
   
-  glShaderSource(shader, 1, (const char**) &source, null);
-  glCompileShader(shader);
+  glShaderSource(shader, 1, (const char**) &source, null); /* copy shader's source to the gpu */
+  glCompileShader(shader);                                 /* compile shader */
 
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &error_code);
-  if (error_code == 0x0)
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &error_code); /* get compilation result */
+  if (error_code == 0x0)                                 /* if result isn't ok */
     {
+      glGetShaderInfoLog(shader, 0x200, null, error_message); /* get logs */
       PRT_ERROR("error, can't compile shader %s\n"
                 "log: %s\n", path, error_message);
       return 0x0;
@@ -243,17 +291,17 @@ shader_program_new(GLuint vert, GLuint frag)
   Char error_message[0x200];
   GLuint prog;
   
-  prog = glCreateProgram();
+  prog = glCreateProgram();     /* create opengl program */
 
-  glAttachShader(prog, vert);
+  glAttachShader(prog, vert);   /* attach two shaders */
   glAttachShader(prog, frag);
 
-  glLinkProgram(prog);
+  glLinkProgram(prog);          /* link shaders in program */
 
-  glGetProgramiv(prog, GL_LINK_STATUS, &error_code);
-  if (error_code == 0x0)
+  glGetProgramiv(prog, GL_LINK_STATUS, &error_code); /* get link status */
+  if (error_code == 0x0)                             /* if status isn't ok */
     {
-      glGetProgramInfoLog(prog, 0x200, null, error_message);
+      glGetProgramInfoLog(prog, 0x200, null, error_message); /* get error message */
       PRT_ERROR("error, can't link shaders to program\n"
                 "log: %s\n", error_message);
       return 0x0;
@@ -268,13 +316,13 @@ shader_program_new_unique(GLuint vert, GLuint frag)
 {
   GLuint prog;
   
-  if ((prog = shader_program_new(vert, frag)) == 0x0)
+  if ((prog = shader_program_new(vert, frag)) == 0x0) /* if can't create program */
     {
       PRT_ERROR(" => error, can't create shader program\n");
       return 0x0;
     }
   
-  glDeleteShader(vert);
+  glDeleteShader(vert);         /* delete two shaders that already was copied */
   glDeleteShader(frag);
   
   return prog;
@@ -289,14 +337,13 @@ main(None)
     shader_vert;
   uint32_t vao, vbo, ebo;
   
-  /* init render system */
-  if (game_system_init() < error_none)
+  if (game_system_init() < error_none) /* init system */
     {
       PRT_ERROR(" => error, can't init game system\n");
       return error_upper;
     }
 
-  if (game_options_init() < error_none)
+  if (game_options_init() < error_none) /* init options */
     {
       PRT_ERROR(" => error, can't init game options\n");
       return error_upper;
@@ -306,7 +353,7 @@ main(None)
 
   glClearColor(0x00, 0xff, 0xff, 0xff); /* set clear color */
 
-  /* Shaders */
+  /* read shaders */
   shader_vert = read_shader(GL_VERTEX_SHADER, "./shader.vert");
   shader_frag = read_shader(GL_FRAGMENT_SHADER, "./shader.frag");
 
@@ -347,21 +394,21 @@ main(None)
   
   
   game_options->wireframe_mode = 0x1; /* enable wireframe mode */
+  game_options->is_game_running = 0x1; /* start game */
+
+  game_reload_all();            /* apply all options */
 
   /* main loop */
-  while (glfwWindowShouldClose(game_system->window) != GLFW_TRUE)
+  while (game_options->is_game_running)
     {
+      game_options->is_game_running &= /* simple, if not is_run == true and (should_close == false) then exit */
+        glfwWindowShouldClose(game_system->window) == GLFW_FALSE;
+      
       glClear(GL_COLOR_BUFFER_BIT); /* clear background */
 
       /* main code */
-      glUseProgram(shader_program);
-      glBindVertexArray(vao);
-      
-      /* control wireframe mode */
-      if (game_options->wireframe_mode != 0x0)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); /* don't fill polygons */
-      else
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); /* fill polygons */
+      glUseProgram(shader_program); /* use shader program */
+      glBindVertexArray(vao);       /* bind vao */
       
       glDrawElements(GL_TRIANGLES, 0x6, GL_UNSIGNED_INT, 0x0);
       
@@ -374,5 +421,4 @@ main(None)
   
   return error_none;
 }
-
 
