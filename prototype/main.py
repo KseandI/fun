935504aa7ccef8838206c894b91cfb151a1e08f4
c4llv07e <kseandi@gmail.com>
2022-02-11 19:08:40 +0300

    added some classes
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- prototype/main.py
+++ prototype/main.py
@@ -5,8 +5,33 @@ from pygame.locals import *
 from pygame.math import Vector2
 
 
+class PgameMan:
+    def __init__(self,
+                 size: (int, int) = (640, 360),
+                 name: str = "window"):
+        """ for variable declorations """
+        self.surface: pygame.Surface
+        self.init(size, name)
+
+        
+    def init(self, size, name):
+        pygame.init()
+        surface = pygame.display.set_mode(size)
+        pygame.display.set_caption(name)
+
+
+class Entity():
+    def __init__(self):
+        pass
+
+class Camera(Entity):
+    def __init__(self):
+        super().__init__()
+    
+
+
 def main():
-    pygame.init()
+    pman = PgameMan((640, 360), "fun")
 
 
 if __name__ == "__main__":
