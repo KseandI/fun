07b346e1f298210dc7356a396ee588989d47be1d
c4llv07e <kseandi@gmail.com>
2022-02-11 19:24:09 +0300

    added main game loop
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- prototype/main.py
+++ prototype/main.py
@@ -16,23 +16,69 @@ class PgameMan:
         
     def init(self, size, name):
         pygame.init()
-        surface = pygame.display.set_mode(size)
+        self.surface = pygame.display.set_mode(size)
         pygame.display.set_caption(name)
+        return
 
 
 class Entity():
     def __init__(self):
-        pass
+        return
+
+
+class DrawEntity(Entity):
+    def __init__(self):
+        super().__init__()
+        self.surface: pygame.Surface
+        self.rect: pygame.rect
+        self.init()
+        return
+
+    def init(self):
+        self.surface = pygame.Surface((16, 16))
+        self.surface.fill((0, 255, 0))
+        self.rect = self.surface.get_rect()
+        return
+
 
 class Camera(Entity):
     def __init__(self):
         super().__init__()
-    
+        self.init()
+        return
+
+    def init(self):
+        return
+
+
+class Game():
+    def __init__(self,
+                 camera: Camera):
+        self.camera: Camera
+        self.is_running: bool
+        self.init(camera)
+
+    def init(self, camera):
+        self.camera = camera
+        self.is_running = True
 
 
 def main():
     pman = PgameMan((640, 360), "fun")
+    camera = Camera()
+    ent = DrawEntity()
+    game = Game(camera)
+
+    while game.is_running:
+
+        # code goes here
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                game.is_running = False
 
 
 if __name__ == "__main__":
     main()
+
+
