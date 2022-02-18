5dd3b949431b97d4419563f46aeeadf18862274f
c4llv07e <kseandi@gmail.com>
2022-02-18 20:27:42 +0300

    added basic prototype
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- prototype/main.lua
+++ prototype/main.lua
@@ -2,15 +2,25 @@
 
 local win =
    {
-      ["weight"] = 0,
+      ["width"] = 0,
       ["height"] = 0,
    }
 
+local player =
+   {
+      ["x"] = 0,
+      ["y"] = 0,
+   }
+
 function love.load()
-   love.graphics.setBackgroundColor(0, 255, 255)
    
-   win["width"]  = love.graphics.getWidth()
-   win["height"] = love.graphics.getHeight()
+   win.width  = love.graphics.getWidth()
+   win.height = love.graphics.getHeight()
+
+end
+
+function love.keypressed(key, unicode)
+   
 end
 
 function love.update(dt)
@@ -18,5 +28,8 @@ function love.update(dt)
 end
 
 function love.draw()
-   
+   love.graphics.setBackgroundColor(0, 255, 255)
+
+   love.graphics.setColor(255, 0, 0)
+   love.graphics.rectangle("fill", player.x, player.y, 32, 32)
 end
