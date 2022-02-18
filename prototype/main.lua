14c4c4af7db0ea2d8aefcf411f7022e65b5f5a2b
c4llv07e <kseandi@gmail.com>
2022-02-18 20:15:16 +0300

    --дум-- love2d, пацаны
    Signed-off-by: c4llv07e <kseandi@gmail.com>

---
+++ prototype/main.lua
@@ -0,0 +1,22 @@
+
+
+local win =
+   {
+      ["weight"] = 0,
+      ["height"] = 0,
+   }
+
+function love.load()
+   love.graphics.setBackgroundColor(0, 255, 255)
+   
+   win["width"]  = love.graphics.getWidth()
+   win["height"] = love.graphics.getHeight()
+end
+
+function love.update(dt)
+   
+end
+
+function love.draw()
+   
+end
